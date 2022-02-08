be3c95ca4d35332a83e2d8b9423feaf2ed728954
Preston Windfeldt <prestonwindfeldt@gmail.com>
2022-02-08 13:33:44 -0700

    test start

--- test.cpp
+++ test.cpp
@@ -1,1 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
 
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
