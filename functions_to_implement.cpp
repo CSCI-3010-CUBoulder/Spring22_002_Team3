c3fc355f71f70bd71610edfe973752bcc195ec30
Preston Windfeldt <prestonwindfeldt@gmail.com>
2022-02-08 12:47:52 -0700

    NthFib and Factorial implementation

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -91,10 +91,16 @@ int Product( vector<int> nums);
  vector<int> SquaresUntil(int n);
 
 // takes an int, n, and returns the nth value of the fibonacci sequence (1, 1, 2, 3, 5, 8, 13, ...)
-int NthFibonacci(int n);
+int NthFibonacci(int n) {
+    if(n == 0 || n == 1) return 1;
+    return NthFibonacci(n-1) + NthFibonacci(n-2);
+}
 
 // takes an int, n, and returns the factorial of that int (n!)
-int Factorial(int n);
+int Factorial(int n) {
+    if(n < 1) return 1;
+    return n * Factorial(n - 1);
+}
 
 // returns -1 if the number is negative and 1 if positive
 int Sign(int num);
