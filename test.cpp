b14841d483cad281e25606a396cec2723b5cbd08
Arjun Lakshmi Narasimhan <larjun01@cu-engr2-1-15-10.201.58.12.int.colorado.edu>
2022-02-08 13:44:23 -0700

    Test does not work

--- test.cpp
+++ test.cpp
@@ -1,1 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
 
+TEST_CASE ( "Removing substring", "[RemoveAllSubstrings]" ) {
+    REQUIRE( RemoveAllSubstrings("Bananaaa", "na"));
+    REQUIRE( RemoveAllSubstrings("Panama", "na"));
+}
